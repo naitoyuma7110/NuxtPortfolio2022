252d056e34cfd3e605a552b185aa13e4d25a3f89
naitoyuma3230 <naitoyuma@outlook.jp>
2022-07-22 16:01:07 +0900

    Nuxt.config をtarget statickに変更

---
+++ docs/_nuxt/static/1658473211/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FNuxtPortfolio\u002F_nuxt\u002Fstatic\u002F1658473211",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FNuxtPortfolio\u002F",assetsPath:"\u002FNuxtPortfolio\u002F_nuxt\u002F",cdnURL:a}}}}(null));
--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,7 @@
 import colors from 'vuetify/es5/util/colors'
 
 export default {
+  target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     titleTemplate: '%s - portfolio',
@@ -72,7 +73,7 @@ export default {
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: { dir: 'docs' },
+  // build: { dir: 'docs' },
   router: {
     base: '/NuxtPortfolio/',
   },
