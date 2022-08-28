76b93281a9a0063a47fce02947525c1808229e1e
naitoyuma3230 <naitoyuma@outlook.jp>
2022-08-28 21:10:54 +0900

    run generate 忘れ .mdファイルの場所を変更をDistへ

--- docs/_nuxt/static/1660801452/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002FNuxtPortfolio\u002F_nuxt\u002Fstatic\u002F1660801452",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002FNuxtPortfolio\u002F",assetsPath:"\u002FNuxtPortfolio\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"27b92962"}}}}(null));
